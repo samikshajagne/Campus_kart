35780fd62be389d05e99e08b1b895e79fecc87dc
samikshajagne <samikshajagne2@gmail.com>
2024-04-06 23:11:38 +0530

    this is my 4  commit.

--- lib/SignupScreen/body.dart
+++ lib/SignupScreen/body.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:campus_cart/SignupScreen/signupbackground.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpBody extends StatefulWidget {
+
 
   @override
   State<SignUpBody> createState() => _SignUpBodyState();
@@ -12,6 +15,86 @@ class SignUpBody extends StatefulWidget {
 class _SignUpBodyState extends State<SignUpBody> {
   File? _image;
   final Signupformkey = GlobalKey<FormState>();
+
+  void _getfromcamera() async
+  {
+    XFile? pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
+    _cropimage(pickedFile?.path);
+    Navigator.pop(context);
+  }
+
+  void _getfromgallery() async
+  {
+    XFile? pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    _cropimage(pickedFile?.path);
+    Navigator.pop(context);
+  }
+
+  void _cropimage(filePath) async
+  {
+    CroppedFile? croppedImage = await ImageCropper().cropImage(sourcePath: filePath, maxHeight: 1080, maxWidth: 1080);
+    if(croppedImage != null)
+      {
+        setState(() {
+          _image = File(croppedImage.path);
+        });
+      }
+  }
+
+  void _showImageDialog()
+  {
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text('Please choose an option'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              InkWell(
+              onTap: ()
+              {
+                _getfromcamera();
+              },
+                child: Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(4.0),
+                      child: Icon(
+                        Icons.camera,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    Text('Camera')
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: ()
+                {
+                  _getfromgallery();
+                },
+                child: Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(4.0),
+                      child: Icon(
+                        Icons.image,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    Text('Gallery',
+                    style: TextStyle(
+                      color: Colors.purple,
+                    ),)
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+
+      }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     double screenwidth = MediaQuery.of(context).size.width,
@@ -23,7 +106,7 @@ class _SignUpBodyState extends State<SignUpBody> {
             Form(key: Signupformkey,
             child: InkWell(
               onTap: (){
-
+                  _showImageDialog();
               },
               child: CircleAvatar(
                 radius: screenwidth * 0.20 ,
@@ -37,7 +120,8 @@ class _SignUpBodyState extends State<SignUpBody> {
                 )
                     : null,
               ),
-            ),),
+            ),
+            ),
           ],
       ),
     ));
